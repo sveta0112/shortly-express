9faebf110de401e9e4a8d94da19230e109fd9e76
sveta0112 <sveta0112@hotmail.com>
2018-10-12 19:17:06 -0700

    Worked on middleware functions, session& cookieParser

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,6 +2,24 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
+  //models.Users.get(id: req.body.id)
+  //models.Sessions.create();
+  var cookies = req.cookies; //cookies object
+  if (Object.keys(cookies).length === 0) { //if object is cookies object is empty
+    models.Sessions.create() //create body
+      .then(result => {
+        
+        // We want to get the hash so use the insert ID
+        return models.Sessions.get({id : result.insertId});
+      })
+      .then(session => {
+        var hash = session.hash;
+        cookies['shortlyid'] = {value: hash};
+        res.cookies = cookies;
+        req.session = {hash};
+        next();
+      });
+  }
 };
 
 /************************************************************/
--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,10 @@
+var cookie = require('cookie');
 const parseCookies = (req, res, next) => {
+  // get req.headers.cookies
+  // parse into object unless there is no cookie
+  var cookies = req.headers.cookie;
+  req.cookies = cookies ? cookie.parse(cookies) : {};
+  next();
 };
 
 module.exports = parseCookies;
