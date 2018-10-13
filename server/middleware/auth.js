bf592cf4201b096456d2a82651762df1a39d5d32
sveta0112 <sveta0112@hotmail.com>
2018-10-12 20:10:15 -0700

    Finish middleware function

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,23 +1,38 @@
 const models = require('../models');
 const Promise = require('bluebird');
 
+// Helper function to create a new session and to set the request and response.
+var newSessionPromise = function(req, res, next) {
+  return models.Sessions.create() //create session
+    .then(result => {
+      
+      // We want to get the hash so use the insert ID
+      return models.Sessions.get({id: result.insertId});
+    })
+    .then(session => {
+      var hash = session.hash;
+      req.cookies['shortlyid'] = {value: hash};
+      res.cookies = req.cookies; // Shallow copy, if bugs occur, copy the cookies object
+      req.session = {hash};
+      next();
+    });
+};
+
 module.exports.createSession = (req, res, next) => {
-  //models.Users.get(id: req.body.id)
-  //models.Sessions.create();
-  var cookies = req.cookies; //cookies object
-  if (Object.keys(cookies).length === 0) { //if object is cookies object is empty
-    models.Sessions.create() //create body
-      .then(result => {
-        
-        // We want to get the hash so use the insert ID
-        return models.Sessions.get({id : result.insertId});
-      })
+  if (Object.keys(req.cookies).length === 0) { //if object is cookies object is empty
+    newSessionPromise(req, res, next);
+  } else {
+    // got an id. Note that in request cookies it is not in a value property
+    var hash = req.cookies.shortlyid;
+    // id in DB
+    models.Sessions.get({hash})
       .then(session => {
-        var hash = session.hash;
-        cookies['shortlyid'] = {value: hash};
-        res.cookies = cookies;
-        req.session = {hash};
-        next();
+        if (session) {
+          req.session = session;
+          next();
+        } else {
+          newSessionPromise(req, res, next);
+        }
       });
   }
 };
